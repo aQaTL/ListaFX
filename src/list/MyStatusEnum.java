c899b19a32cf33bcaef27864907d670e00671e9d
Maciej <mmsoltys@outlook.com>
2016-06-10 22:17:26 +0200

    Refactored seriesStatus from String to MyStatusEnum

---
+++ src/list/MyStatusEnum.java
@@ -0,0 +1,45 @@
+package list;
+
+/**
+ * Created by Maciej on 2016-05-31.
+ */
+public enum MyStatusEnum
+{
+	WATCHING(1), COMPLETED(2), ONHOLD(3), DROPPED(4), PLANTOWATCH(6);
+
+	private int statusNumber;
+
+	public int getStatusNumber()
+	{
+		return statusNumber;
+	}
+
+	public static MyStatusEnum getMyStatusEnum(String statusNumber)
+	{
+		switch (statusNumber)
+		{
+			case "1":
+				return WATCHING;
+
+			case "2":
+				return COMPLETED;
+
+			case "3":
+				return ONHOLD;
+
+			case "4":
+				return DROPPED;
+
+			case "6":
+				return PLANTOWATCH;
+
+			default:
+				return null;
+		}
+	}
+
+	MyStatusEnum(int status)
+	{
+		this.statusNumber = status;
+	}
+}
